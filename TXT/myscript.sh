f2612a95efde90313cd5a1aabe6bf78439ba0648
Kaloosh Falito Verrell <96697117+kalooshfv@users.noreply.github.com>
2022-09-16 06:46:40 +0700

    Update myscript.sh

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -12,7 +12,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="60914D29C01C81F1"
+REC2="AE6EF083730019BDB"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
